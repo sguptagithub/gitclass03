6b7d24e7b916e4abfd594a14e81b189a449c0ee3
sguptagithub <sunitagupta1@gmail.com>
2023-04-11 20:52:55 -0400

    second commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("i am a project");
+        System.out.println("i am a continuation to project");
+    }
+}
